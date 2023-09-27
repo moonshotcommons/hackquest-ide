7c0c8a37d7efbf4e974096c693b2de18fe026d91
zcy22606 <619730086@qq.com>
2023-09-27 11:52:58 +0800

    feat: layout update

---
+++ libs/remix-ui/app/src/lib/remix-app/components/footer/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC, ReactNode } from 'react'
+import './footer.style.css'
+const HackquestFooter: FC = () => {
+  return <div className="hackquest-footer">HackQuest IDE</div>
+}
+
+export default HackquestFooter
