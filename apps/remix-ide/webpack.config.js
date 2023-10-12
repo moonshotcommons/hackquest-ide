035c718e9967ce26d31a0d727552150b694fdd3e
zcy22606 <619730086@qq.com>
2023-10-12 19:35:19 +0800

    feat: update link config

--- apps/remix-ide/webpack.config.js
+++ apps/remix-ide/webpack.config.js
@@ -11,10 +11,10 @@ const axios = require('axios')
 const getEnv = () => {
   switch (process.env.NODE_ENV) {
     case 'staging':
+      return 'staging'
+    case 'prod':
       return 'production'
-    case 'production':
-      return 'production'
-    case 'development':
+    case 'dev':
     default:
       return 'development'
   }
